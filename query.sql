4435c7251f1c999cd731f511b2928a1cb4b0742d
alimran-dev <alimran5air@gmail.com>
2026-06-15 23:28:11 +0600

    added: query 2

--- query.sql
+++ query.sql
@@ -1,3 +1,8 @@
 -- Query 1
 select match_id, fixture, base_ticket_price from matches
 where tournament_category = 'Champions League' and match_status = 'Available';
+
+
+-- Query 2
+select user_id, full_name, email from users
+where full_name ilike 'Tanvir%' or full_name ilike '%Haque%';
