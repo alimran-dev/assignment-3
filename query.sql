2ea168983a72671a5e4fc2b9e06d4cc66d63e829
alimran-dev <alimran5air@gmail.com>
2026-06-15 23:45:44 +0600

    added: query 7

--- query.sql
+++ query.sql
@@ -29,3 +29,9 @@ select booking_id, match_id, total_cost from bookings
 where total_cost > (
   select avg(total_cost) from bookings
 );
+
+
+-- Query 7
+select match_id, fixture, base_ticket_price from matches
+order by base_ticket_price desc
+limit 2 offset 1;
